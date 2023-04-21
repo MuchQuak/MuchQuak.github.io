89f0097f55d6eead4978c98000109cbc488dd59f
MuchQuak <loganpwilt@gmail.com>
2023-04-20 23:21:16 -0700

    migrating css to tailwind, changed gradient colors to match ufei themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+      colors: {
+         transparent: 'transparent',
+         current: 'currentColor',
+         black: '#212529',
+         white: colors.white,
+         gray: colors.gray,
+         emerald: colors.emerald,
+         indigo: colors.indigo,
+         yellow: colors.yellow,
+         'se': '#DFDFDF',
+         //'gray': '#DFDFDF',
+         'green': '#457537'
+      },
 		extend: {},
 	},
 	plugins: [],
