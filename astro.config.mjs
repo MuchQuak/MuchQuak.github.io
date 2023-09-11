bd101ff8b538c449b7f1ce77e73ad432e1109275
MuchQuak <loganpwilt@gmail.com>
2023-09-11 13:18:08 -0700

    Added apline, upgrade to 3.0, new banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), alpinejs()],
   site: 'https://muchquak.github.io'
 });
